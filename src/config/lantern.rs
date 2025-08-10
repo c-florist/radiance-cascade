9f2ccc29d0b4faf7920d4cb518641aa39c27c6f0
Connor Flowers <cjflowers@pm.me>
2025-08-10 12:09:02 +1000

    reset configs - new build

--- src/config/lantern.rs
+++ src/config/lantern.rs
@@ -1,14 +1,4 @@
 use bevy::prelude::Resource;
 
-#[derive(Resource)]
-pub struct LanternConfig {
-    pub personal_space: f32,
-}
-
-impl Default for LanternConfig {
-    fn default() -> Self {
-        Self {
-            personal_space: 0.5,
-        }
-    }
-}
+#[derive(Resource, Default)]
+pub struct LanternConfig;
--- src/config/moth.rs
+++ src/config/moth.rs
@@ -2,15 +2,8 @@ use bevy::prelude::Resource;
 
 #[derive(Resource)]
 pub struct MothConfig {
-    pub moth_count: usize,
+    pub moth_count: i32,
     pub moth_speed: f32,
-
-    pub wander_factor: f32,
-
-    // --- Landing ---
-    pub landing_distance: f32,
-    pub landing_chance: f64,
-    pub landed_duration_secs: f32,
 }
 
 impl Default for MothConfig {
@@ -18,10 +11,6 @@ impl Default for MothConfig {
         Self {
             moth_count: 150,
             moth_speed: 1.0,
-            wander_factor: 0.1,
-            landing_distance: 0.6,
-            landing_chance: 0.08,
-            landed_duration_secs: 3.0,
         }
     }
 }
